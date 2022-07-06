793d67b4206dc4b383862a1030da5b854b8f16be
gabriellichacz <gabriellichacz@gmail.com>
2022-07-06 11:27:24 +0200

    Graficzna obróbka templatki

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,9 +85,31 @@
         </nav>
         <!-- end of navigation -->
 
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase"> Pogoda </h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- end of Masthead -->
+
+        <!-- Main -->
         <main class="py-4">
             @yield('content')
         </main>
+        <!-- end of Main -->
+
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50">
+            <div class="container px-4 px-lg-5">
+                <p class="p-small"> @<?php echo date("Y"); ?> </p>
+            </div>
+        </footer>
+        <!-- end of Footer-->
 
     </div>
 </body>

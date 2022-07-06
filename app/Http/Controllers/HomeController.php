fb9bfe54197ee80a8f437ef34c46b74cda2d633a
gabriellichacz <gabriellichacz@gmail.com>
2022-07-06 13:26:04 +0200

    Dzialajace wywolanie API

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,24 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home');
@@ -28,8 +19,44 @@ class HomeController extends Controller
 
     public function APIcall()
     {
+        // Data
+        $city_id = "3247463"; // will be dynamic
+        $API_Key = 'f7df02ae6a92e103bdc3996cbf4099a5';
 
+        // Calling API
+        $apiData = @file_get_contents('https://api.openweathermap.org/data/2.5/weather?id='.$city_id.'&appid='.$API_Key.'');
+        if($apiData) {
+            $data = json_decode($apiData); 
+        }
+        else { // if data is not found
+            echo 'Invalid API key or invalid city ID.'; 
+        }
 
-        //return redirect('home');
+        // Extracting data - static
+        $data_weather = [];
+        $data_weather[0] = $data->main->temp;
+        $data_weather[1] = $data->main->feels_like;
+        $data_weather[2] = $data->main->temp_min;
+        $data_weather[3] = $data->main->temp_max;
+        $data_weather[4] = $data->main->pressure;
+        $data_weather[5] = $data->main->humidity;
+
+        // Extracting data - array
+        /*$data_strings = ['temp','feels_like','temp_min','temp_max','pressure','humidity'];
+
+        for ($i = 0; $i <= count($data_strings)-1; $i++) {
+            $data_weather[$i] = $data-> main -> $data_strings[$i]; // Array to string conversion ERROR
+        }
+        */
+
+        //dd($data_weather); // dump data
+        return ([
+            'temp' => $data_weather[0],
+            'feels_like' => $data_weather[1],
+            'temp_min' => $data_weather[2],
+            'temp_max' => $data_weather[3],
+            'pressure' => $data_weather[4],
+            'humidity' => $data_weather[5],
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container bg-dark-custom">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header text-center">{{ __('Dashboard') }}</div>
 
-                <div class="card-body text-center">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('Jesteś zalogowany') }}
+
+<div class="container-fluid text-white-50 bg-dark-custom">
+  <div class="px-lg-3">
+
+    <!-- Header -->
+    <div class="row py-5">
+      <div class="col-lg-12 mx-auto">
+        <div class="text-black p-3 shadow-sm rounded banner text-center text-white-50">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+          <h4 class="display-3"> {{ __('Jesteś zalogowany') }} </h1>
         </div>
+      </div>
     </div>
+    <!-- End of header -->
+
+    <!-- Cards -->
+    <div class="row"> 
+
+      <!-- City card -->
+      <div class="col-xl-4 col-lg-3 col-md-6 mb-4">
+        <div class="p-2">
+          <div class="rounded shadow-sm">
+            <img src="#" class="img-fluid card-img-top" alt="City">
+          </div>
+        </div>
+        <div class="p-3 text-white-50 text-center">
+          <h4 class="text-decoration-none"> Nazwa miasta </h4>
+          <p class="mb-0"> Temperatura: </p>
+          <p class="mb-0"> Temperatura odczuwalna: </p>
+          <p class="mb-0"> Temperatura min: </p>
+          <p class="mb-0"> Temperatura max: </p>
+          <p class="mb-0"> Wilgotność: </p>
+        </div>
+      </div>
+      <!-- End of City card -->
+
+      <!-- City card -->
+      <div class="col-xl-4 col-lg-3 col-md-6 mb-4">
+        <div class="p-2">
+          <div class="rounded shadow-sm">
+            <img src="#" class="img-fluid card-img-top" alt="City">
+          </div>
+        </div>
+        <div class="p-3 text-white-50 text-center">
+          <h4 class="text-decoration-none"> Nazwa miasta </h4>
+          <p class="mb-0"> Temperatura: </p>
+          <p class="mb-0"> Temperatura odczuwalna: </p>
+          <p class="mb-0"> Temperatura min: </p>
+          <p class="mb-0"> Temperatura max: </p>
+          <p class="mb-0"> Wilgotność: </p>
+        </div>
+      </div>
+      <!-- End of City card -->
+
+      <!-- City card -->
+      <div class="col-xl-4 col-lg-3 col-md-6 mb-4">
+        <div class="p-2">
+          <div class="rounded shadow-sm">
+            <img src="#" class="img-fluid card-img-top" alt="City">
+          </div>
+        </div>
+        <div class="p-3 text-white-50 text-center">
+          <h4 class="text-decoration-none"> Nazwa miasta </h4>
+          <p class="mb-0"> Temperatura: </p>
+          <p class="mb-0"> Temperatura odczuwalna: </p>
+          <p class="mb-0"> Temperatura min: </p>
+          <p class="mb-0"> Temperatura max: </p>
+          <p class="mb-0"> Wilgotność: </p>
+        </div>
+      </div>
+      <!-- End of City card -->
+
+
+    </div>
+    <!-- end of Cards -->
+
+  </div>
 </div>
 
 @endsection
